a000cd88e5dcd360f138aa7a03785e4cc9cbe54e
Bony Mondal <bony.mondal.17@mountblue.tech>
2021-11-06 15:49:22 +0530

    refactor: changed plot axis

--- problem3.py
+++ problem3.py
@@ -20,6 +20,6 @@ number_of_umpires=umpire_is_not_indian.groupby('Nationality')[['Umpire']].count(
 # plotting dataset
 sns.set_style("darkgrid")
 plt.title("Numbers of Umpires from different countries.")
-sns.barplot(x=number_of_umpires.index,
-            y=number_of_umpires.Umpire)
+sns.barplot(y=number_of_umpires.index,
+            x=number_of_umpires.Umpire)
 plt.savefig('./output/problem_3.png')
