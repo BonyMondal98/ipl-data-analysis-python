c767fd81db8c598a00a0b7dcce00fb182768a016
Bony Mondal <bony.mondal.17@mountblue.tech>
2021-11-09 18:05:26 +0530

    added label for x and y axis

--- additional/problem6.py
+++ additional/problem6.py
@@ -4,13 +4,22 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 runs_by_player=dict()
+
 balls_by_player=dict()
+
 values_of_runs_by_player=[]
+
 values_of_balls_by_player=[]
+
 names_of_players=[]
+
 cache=[]
-res=[]
-result=dict()
+
+pair_of_names_values=[]
+
+result_dictionary_for_plot=dict()
+
+# Reading files without pandas module
 matches=open('./data/matches.csv', 'r')
 deliveries=open('./data/deliveries.csv', 'r')
 
@@ -37,6 +46,7 @@ for deliver in my_deliveries:
                 balls_by_player[deliver[8]]=1
             if int(deliver[13])>0 or int(deliver[10])>0:
                 balls_by_player[deliver[8]]-=1
+
 for key,value in runs_by_player.items():
     names_of_players.append(key)
     values_of_runs_by_player.append(value)
@@ -47,22 +57,27 @@ for i in range(len(values_of_balls_by_player)):
     cache.append("{:.2f}".format(values_of_runs_by_player[i]/(values_of_balls_by_player[i]/6)))
 
 for i in range(len(names_of_players)):
-    res.append([names_of_players[i],cache[i]])
+    pair_of_names_values.append([names_of_players[i],cache[i]])
 
-for i in res:
-    if i[0] not in result:
-        result[i[0]]=i[1]
+for i in pair_of_names_values:
+    if i[0] not in result_dictionary_for_plot:
+        result_dictionary_for_plot[i[0]]=i[1]
 
-top_10_economical_bowlers=sorted((float(y),x) for x,y in result.items())
+top_10_economical_bowlers=sorted((float(y),x) for x,y in result_dictionary_for_plot.items())
 top_10_economical_bowlers_dict=dict()
 top_10_economical_bowlers_slice=slice(0,10)
 for i in top_10_economical_bowlers[top_10_economical_bowlers_slice]:
     if i[1] not in top_10_economical_bowlers_dict:
         top_10_economical_bowlers_dict[i[1]]=i[0]
+
+# plot for top 10 economical bowlers
 sns.set_style("darkgrid")
 plt.figure(figsize=(12, 7))
 plt.title("Extra runs conceded per team in the year 2016")
+plt.xlabel("Bowlers")
+plt.ylabel("Economy")
 plt.bar(*zip(*top_10_economical_bowlers_dict.items()))
+plt.xticks(rotation=-10)
 plt.savefig("./output/problem_6.png")
 
 
